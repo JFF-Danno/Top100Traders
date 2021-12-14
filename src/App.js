654c2e87cea4ce14466292440167b2a2cc66b045
LilFatFrank <karan.parsnani@ymail.com>
2021-12-14 20:01:55 +0530

    changing alignment, and adding styling

--- src/App.js
+++ src/App.js
@@ -1,70 +1,162 @@
-import React, { useState , useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
-import './App.css';
-import axios from 'axios';
-import Banner from './images/banner.png';
-import Row from './images/row.png';
-import rank1 from './images/1st.png';
-import rank2 from './images/2nd.png';
-import rank3 from './images/3rd.png';
-import follow from './images/follow.png';
-import address from './images/Address.png';
-import rank from './images/Rank.png';
-import buys from './images/buys.png';
-import avgprice from './images/avgprice.png';
-import sold from './images/sold.png';
-import profit from './images/Profit.png';
-import roi from './images/ROI.png';
-import genesis from './images/Genesis.png';
-import ethereum from './images/eth.png';
-import solana from './images/solana.png';
-import bnb from './images/bnb.png';
-import exportimg from './images/export.png';
+import "./App.css";
+import axios from "axios";
+import Banner from "./images/banner.png";
+import Row from "./images/row.png";
+import rank1 from "./images/1st.png";
+import rank2 from "./images/2nd.png";
+import rank3 from "./images/3rd.png";
+import follow from "./images/follow.png";
+import address from "./images/Address.png";
+import rank from "./images/Rank.png";
+import buys from "./images/buys.png";
+import avgprice from "./images/avgprice.png";
+import sold from "./images/sold.png";
+import profit from "./images/Profit.png";
+import roi from "./images/ROI.png";
+import genesis from "./images/Genesis.png";
+import ethereum from "./images/eth.png";
+import solana from "./images/solana.png";
+import bnb from "./images/bnb.png";
+import exportimg from "./images/export.png";
 
 function App() {
-
-   const [ data,     setData ] = useState([]);
-    const gettop50 = async () => {
-      const resMint = await axios.get( 'http://localhost:8080/Top100?Action=localTop100', {
-//     const resMint = await axios.get( 'data.json', {
-       headers: { "Content-Type": "application/json",
-        },   
-      });
-     // console.log(resMint.data[0]);
-      let accountMetaData = [];
-
-      for( let i = 0; i <  resMint.data.length; i++ ) {
-        accountMetaData.push(resMint.data[i]);
+  const [data, setData] = useState([]);
+  const gettop50 = async () => {
+    const resMint = await axios.get(
+      "http://localhost:8080/Top100?Action=localTop100",
+      {
+        //     const resMint = await axios.get( 'data.json', {
+        headers: { "Content-Type": "application/json" }
       }
-      const rows = accountMetaData.map((row,index) => 
-        
-          <li className='row' ><span className='rank' ><div className='contentDiv'>{row.rank == 1 ? <img src={rank1} />: row.rank == 2 ? <img src={rank2} /> : row.rank == 3 ? <img src={rank3} /> : row.rank}</div></span><span className='link' ><div className='contentDiv'><a target='_blank' href={row.link}>{row.link.replace('https://opensea.io/','').substring(0,6)}...{row.link.replace('https://opensea.io/','').substring(37)}</a></div></span><span className='buy' ><div className='contentDiv'>{row.buys}</div></span><span className='avgprice' ><div className='contentDiv'>$ {row.avgprice}</div></span><span className='sold' ><div className='contentDiv'>{row.sold}</div></span><span className='profit' ><div className='contentDiv'>$ {row.profit}</div></span><span className='roi' ><div className='contentDiv'>{row.roi}</div></span><span className='genesis' ><div className='contentDiv'>{row.genesis.substring(0,10)}</div></span></li>
-            
-      );
-      setData(rows);
-  }
-  
-  
-   useEffect(() => {
-   
-    gettop50()
-            
+    );
+    // console.log(resMint.data[0]);
+    let accountMetaData = [];
+
+    for (let i = 0; i < resMint.data.length; i++) {
+      accountMetaData.push(resMint.data[i]);
+    }
+    const rows = accountMetaData.map((row, index) => (
+      <li className="row">
+        <span className="rank">
+          <div className="contentDiv">
+            {row.rank == 1 ? (
+              <img src={rank1} />
+            ) : row.rank == 2 ? (
+              <img src={rank2} />
+            ) : row.rank == 3 ? (
+              <img src={rank3} />
+            ) : (
+              row.rank
+            )}
+          </div>
+        </span>
+        <span className="link">
+          <div className="contentDiv">
+            <a target="_blank" href={row.link}>
+              {row.link.replace("https://opensea.io/", "").substring(0, 6)}...
+              {row.link.replace("https://opensea.io/", "").substring(37)}
+            </a>
+          </div>
+        </span>
+        <span className="buy">
+          <div className="contentDiv">{row.buys}</div>
+        </span>
+        <span className="avgprice">
+          <div className="contentDiv">$ {row.avgprice}</div>
+        </span>
+        <span className="sold">
+          <div className="contentDiv">{row.sold}</div>
+        </span>
+        <span className="profit">
+          <div className="contentDiv">$ {row.profit}</div>
+        </span>
+        <span className="roi">
+          <div className="contentDiv">{row.roi}</div>
+        </span>
+        <span className="genesis">
+          <div className="contentDiv">{row.genesis.substring(0, 10)}</div>
+        </span>
+      </li>
+    ));
+    setData(rows);
+  };
+
+  useEffect(() => {
+    gettop50();
+
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
-  
+  }, []);
 
   return (
     <div className="App">
-    <div><img src={Banner} /></div>
-  <div>
-    <ul>
-      <li id='toprow' ><span id='topleft' ><span id='eth'><img src={ethereum} /></span><span id='solana'><img src={solana} /></span><span id='bnb'><img src={bnb} /></span></span><span><img src={exportimg} /></span></li>
-<li className='title row' ><span className='ranktitle' ><div className='contentDiv'><img src={rank} /></div></span><span className='link' ><div className='contentDiv'><img src={address} /></div></span><span className='buy' ><div className='contentDiv'><img src={buys} /></div></span><span className='avgprice' ><div className='contentDiv'><img src={avgprice} /></div></span><span className='sold' ><div className='contentDiv'><img src={sold} /></div></span><span className='profit' ><div className='contentDiv'><img src={profit} /></div></span><span className='roi' ><div className='contentDiv'><img src={roi} /></div></span><span className='genesis' ><div className='contentDiv'><img src={genesis} /></div></span></li>
-    {data}
-    </ul>    </div>
-
-
-
+      <div>
+        <img src={Banner} alt="banner" style={{ width: "100%" }} />
+      </div>
+      <div>
+        <ul className={`list-align`}>
+          <li id="toprow">
+            <span id="topleft">
+              <span id="eth">
+                <img src={ethereum} alt="ethereum" />
+              </span>
+              <span id="solana">
+                <img src={solana} alt="solana" />
+              </span>
+              <span id="bnb">
+                <img src={bnb} alt="bnb" />
+              </span>
+            </span>
+            <span>
+              <img src={exportimg} alt="exporting" />
+            </span>
+          </li>
+          <li className="title row">
+            <span className="ranktitle">
+              <div className="contentDiv">
+                <img src={rank} alt="rank" />
+              </div>
+            </span>
+            <span className="link">
+              <div className="contentDiv">
+                <img src={address} alt="address" />
+              </div>
+            </span>
+            <span className="buy">
+              <div className="contentDiv">
+                <img src={buys} alt="src" />
+              </div>
+            </span>
+            <span className="avgprice">
+              <div className="contentDiv">
+                <img src={avgprice} alt="avgprice" />
+              </div>
+            </span>
+            <span className="sold">
+              <div className="contentDiv">
+                <img src={sold} alt="sold" />
+              </div>
+            </span>
+            <span className="profit">
+              <div className="contentDiv">
+                <img src={profit} alt="profit" />
+              </div>
+            </span>
+            <span className="roi">
+              <div className="contentDiv">
+                <img src={roi} alt="roi" />
+              </div>
+            </span>
+            <span className="genesis">
+              <div className="contentDiv">
+                <img src={genesis} alt="genesis" />
+              </div>
+            </span>
+          </li>
+          {data}
+        </ul>
+      </div>
     </div>
   );
 }
--- src/ethereum.js
+++ src/ethereum.js
@@ -1,100 +1,212 @@
-import React, { useState , useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import moralis from "moralis";
-import Web3 from 'web3';
-import './App.css';
-import axios from 'axios';
-import Banner from './images/banner.png';
-import Row from './images/row.png';
-import rank1 from './images/1st.png';
-import rank2 from './images/2nd.png';
-import rank3 from './images/3rd.png';
-import follow from './images/follow.png';
-import address from './images/Address.png';
-import rank from './images/Rank.png';
-import buys from './images/buys.png';
-import avgprice from './images/avgprice.png';
-import sold from './images/sold.png';
-import profit from './images/Profit.png';
-import roi from './images/ROI.png';
-import genesis from './images/Genesis.png';
-import ethereum from './images/eth.png';
-import polygon from './images/polygon.png';
-import polygonfade from './images/polygon-fade.png';
-import solana from './images/solana-cs.png';
-import bnb from './images/bnb.png';
-import exportimg from './images/export.png';
-import dune from './images/dune.png';
-import opensea from './images/opensea.png';
-import followtitle from './images/followtitle.png';
-import moralislogo from './images/moralis.png';
+import Web3 from "web3";
+import "./App.css";
+import axios from "axios";
+import Banner from "./images/banner.png";
+import Row from "./images/row.png";
+import rank1 from "./images/1st.png";
+import rank2 from "./images/2nd.png";
+import rank3 from "./images/3rd.png";
+import follow from "./images/follow.png";
+import address from "./images/Address.png";
+import rank from "./images/Rank.png";
+import buys from "./images/buys.png";
+import avgprice from "./images/avgprice.png";
+import sold from "./images/sold.png";
+import profit from "./images/Profit.png";
+import roi from "./images/ROI.png";
+import genesis from "./images/Genesis.png";
+import ethereum from "./images/eth.png";
+import polygon from "./images/polygon.png";
+import polygonfade from "./images/polygon-fade.png";
+import solana from "./images/solana-cs.png";
+import bnb from "./images/bnb.png";
+import exportimg from "./images/export.png";
+import dune from "./images/dune.png";
+import opensea from "./images/opensea.png";
+import followtitle from "./images/followtitle.png";
+import moralislogo from "./images/moralis.png";
 var web3 = new Web3(Web3.givenProvider);
 function App() {
+  const [data, setData] = useState([]);
+  const [usdConversion, setUsdPrice] = useState(0);
 
-   const [ data,          setData       ] = useState([]);
-   const [usdConversion,  setUsdPrice   ] = useState(0);
-   
   const serverUrl = process.env.REACT_APP_MORALIS_SERVER_URL;
   const appId = process.env.REACT_APP_MORALIS_APPLICATION_ID;
   moralis.start({ serverUrl, appId });
-  
-  const getUSDValue = async () => {
-  const options = {
-    address: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
-    chain: "eth",
-    exchange: "uniswap-v3"
-  };
-  const price = await moralis.Web3API.token.getTokenPrice(options);
-  setUsdPrice(price.usdPrice);
- }
- 
- function numberWithCommas(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
- 
- 
-    const gettop50 = async () => {
- //     const resMint = await axios.get( 'http://localhost:8080/Top100?Action=Top100Polygon', {
-     const resMint = await axios.get( 'eth100.json', {
-       headers: { "Content-Type": "application/json",
-        },   
-      });
-     // console.log(resMint.data[0]);
-      let accountMetaData = [];
 
-      for( let i = 0; i <  resMint.data.length; i++ ) {
-        accountMetaData.push(resMint.data[i]);
-      }
-      const rows = accountMetaData.map((row,index) => 
-        
-          <li className='row' ><span className='rank' ><div className='contentDiv'>{row.rank == 1 ? <div className="imgspan">&nbsp;</div>: row.rank == 2 ? <div className="imgspan2">&nbsp;</div> : row.rank == 3 ? <div className="imgspan3">&nbsp;</div> : row.rank}</div></span><span className='link' ><div className='contentDiv'><a target='_blank' href={'https://opensea.io/' +row.link}>{row.link.replace('https://opensea.io/','').substring(0,6)}...{row.link.replace('https://opensea.io/','').substring(37)}</a></div></span><span className='buy' ><div className='contentDiv'>{row.buys}</div></span><span className='avgprice' ><div className='contentDiv'>$ {( row.avgprice * 1 ).toFixed(2) }</div></span><span className='sold' ><div className='contentDiv'>{ row.sold }</div></span><span className='profit' ><div className='contentDiv'>$ { (numberWithCommas( (row.profit * 1 ).toFixed(2) ) ) }</div></span><span className='roi' ><div className='contentDiv'>{(row.roi*1).toFixed(0)} %</div></span><span className='genesis' ><div className='contentDiv'>{ row.genesis }</div></span><span className='follow' ><div className='contentDiv'><a target='_blank' href={'https://opensea.io/' + row.link}><img className='followimg' src={ follow }/></a></div></span></li>
-            
-      );
-      setData(rows);
+  const getUSDValue = async () => {
+    const options = {
+      address: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+      chain: "eth",
+      exchange: "uniswap-v3"
+    };
+    const price = await moralis.Web3API.token.getTokenPrice(options);
+    setUsdPrice(price.usdPrice);
+  };
+
+  function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   }
-  
-  
-   useEffect(() => {
-       getUSDValue();
-    gettop50()
-            
+
+  const gettop50 = async () => {
+    //     const resMint = await axios.get( 'http://localhost:8080/Top100?Action=Top100Polygon', {
+    const resMint = await axios.get("eth100.json", {
+      headers: { "Content-Type": "application/json" }
+    });
+    // console.log(resMint.data[0]);
+    let accountMetaData = [];
+
+    for (let i = 0; i < resMint.data.length; i++) {
+      accountMetaData.push(resMint.data[i]);
+    }
+    const rows = accountMetaData.map((row, index) => (
+      <li className="row">
+        <span className="rank">
+          <div className="contentDiv">
+            {row.rank == 1 ? (
+              <div className="imgspan">&nbsp;</div>
+            ) : row.rank == 2 ? (
+              <div className="imgspan2">&nbsp;</div>
+            ) : row.rank == 3 ? (
+              <div className="imgspan3">&nbsp;</div>
+            ) : (
+              <span style={{ opacity: "72%" }}>{row.rank}</span>
+            )}
+          </div>
+        </span>
+        <span className="link">
+          <div className="contentDiv">
+            <a target="_blank" href={"https://opensea.io/" + row.link} style={{ opacity: "72%" }}>
+              {row.link.replace("https://opensea.io/", "").substring(0, 6)}...
+              {row.link.replace("https://opensea.io/", "").substring(37)}
+            </a>
+          </div>
+        </span>
+        <span className="buy">
+          <div className="contentDiv" style={{ opacity: "72%" }}>{row.buys}</div>
+        </span>
+        <span className="avgprice">
+          <div className="contentDiv" style={{ opacity: "72%" }}>$ {(row.avgprice * 1).toFixed(2)}</div>
+        </span>
+        <span className="sold">
+          <div className="contentDiv" style={{ opacity: "72%" }}>{row.sold}</div>
+        </span>
+        <span className="profit">
+          <div className="contentDiv" style={{ opacity: "72%" }}>
+            $ {numberWithCommas((row.profit * 1).toFixed(2))}
+          </div>
+        </span>
+        <span className="roi">
+          <div className="contentDiv" style={{ opacity: "72%" }}>{(row.roi * 1).toFixed(0)} %</div>
+        </span>
+        <span className="genesis">
+          <div className="contentDiv" style={{ opacity: "72%" }}>{row.genesis}</div>
+        </span>
+        <span className="follow">
+          <div className="contentDiv">
+            <a target="_blank" href={"https://opensea.io/" + row.link}>
+              <img className="followimg" src={follow} />
+            </a>
+          </div>
+        </span>
+      </li>
+    ));
+    setData(rows);
+  };
+
+  useEffect(() => {
+    getUSDValue();
+    gettop50();
+
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [usdConversion])
-  
+  }, [usdConversion]);
 
   return (
     <div className="App">
-    <div><img src={Banner} /></div>
+      <div>
+        <img src={Banner} alt="banner" style={{ width: "100%" }} />
+      </div>
 
-  <div>
-    <ul>
-      <li id='toprow' ><span id='topleft' ><span id='polygon'><a href="#/polygon" ><img src={polygonfade} /></a></span><span id='eth'><img src={ethereum} title="Ethereum"/></span><span id='solana'><img title="Coming Soon!" src={solana} /></span><span id='bnb'><img title="Coming Soon!" src={bnb} /></span></span><span><img src={exportimg} /></span></li>
-<li className='title row' ><span className='rank' ><div className='contentDiv'><img src={rank} /></div></span><span className='link' ><div className='contentDiv'><img src={address} /></div></span><span className='buy' ><div className='contentDiv'><img src={buys} /></div></span><span className='avgprice' ><div className='contentDiv'><img src={avgprice} /></div></span><span className='sold' ><div className='contentDiv'><img src={sold} /></div></span><span className='profit' ><div className='contentDiv'><img src={profit} /></div></span><span className='roi' ><div className='contentDiv'><img src={roi} /></div></span><span className='genesis' ><div className='contentDiv'><img src={genesis} /></div></span><span className='follow' ><div className='contentDiv'><img  src={followtitle} /></div></span></li>
-    {data}
-    </ul>    
-    <div id="footer"><img src={dune} />&nbsp;&nbsp;&nbsp;<img src={moralislogo} /></div></div>
-
-
-
+      <div>
+        <ul className="list-align">
+          <li id="toprow">
+            <span id="topleft">
+              <span id="polygon">
+                <a href="#/polygon">
+                  <img src={polygonfade} />
+                </a>
+              </span>
+              <span id="eth">
+                <img src={ethereum} alt="ethereum" />
+              </span>
+              <span id="solana">
+                <img title="Coming Soon!" src={solana} alt="solana" />
+              </span>
+              <span id="bnb">
+                <img title="Coming Soon!" src={bnb} alt="bnb" />
+              </span>
+            </span>
+            <span>
+              <img src={exportimg} />
+            </span>
+          </li>
+          <li className="title row">
+            <span className="rank">
+              <div className="contentDiv">
+                <img src={rank} />
+              </div>
+            </span>
+            <span className="link">
+              <div className="contentDiv">
+                <img src={address} />
+              </div>
+            </span>
+            <span className="buy">
+              <div className="contentDiv">
+                <img src={buys} />
+              </div>
+            </span>
+            <span className="avgprice">
+              <div className="contentDiv">
+                <img src={avgprice} />
+              </div>
+            </span>
+            <span className="sold">
+              <div className="contentDiv">
+                <img src={sold} />
+              </div>
+            </span>
+            <span className="profit">
+              <div className="contentDiv">
+                <img src={profit} />
+              </div>
+            </span>
+            <span className="roi">
+              <div className="contentDiv">
+                <img src={roi} />
+              </div>
+            </span>
+            <span className="genesis">
+              <div className="contentDiv">
+                <img src={genesis} />
+              </div>
+            </span>
+            <span className="follow">
+              <div className="contentDiv">
+                <img src={followtitle} />
+              </div>
+            </span>
+          </li>
+          {data}
+        </ul>
+        <div id="footer">
+          <img src={dune} />
+          &nbsp;&nbsp;&nbsp;
+          <img src={moralislogo} />
+        </div>
+      </div>
     </div>
   );
 }
